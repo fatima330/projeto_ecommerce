b0dd8132f0c34836d8408f13f3f921fa6562c222
leorvergani <vergani@outlook.com.br>
2024-07-15 01:55:40 -0300

    Treinamento do carrossel

--- script.js
+++ script.js
@@ -6,12 +6,28 @@ let idx = 0;
 function carrossel(){
     idx++;
 
-    if(idx > img.length -1){
+    if(idx > img.length - 1){
         idx = 0;
     }
 
-    imgs.style.transform = 'translateX(${-idx * 500}px)';
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
 
 }
 
 setInterval(carrossel, 1800);
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 3500);
+
+function nextImage(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}

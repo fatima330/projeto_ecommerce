af39db0f6a008dbeb6a58abaa021b635aa579da7
leorvergani <vergani@outlook.com.br>
2024-07-15 02:37:37 -0300

    add slider pag home

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,19 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 3500);
+
+function nextImage(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
